29c33d3ee0599289f5e82c426cd6ec4774707a11
Gerardo Campos <751033@student.dusd.net>
2024-05-30 15:28:32 -0700

    Need to fix movement
    I added the level, background, player. I need to add obstacles, player movement, and the UI

---
+++ GameProgramming1FinalProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+}
